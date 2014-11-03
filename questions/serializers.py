0ef9d080f31954de86282ac339a51a72bfb0eb73
ancharon <ancharon@gmail.com>
2014-11-03 15:16:48 -0500

    update serializers
    -changing the serializer relationship to allow saving answers nested
    within questions to work properly
    -also hacked in a link back to the parent object -- need to look into
    cleaner way to handle this

--- questions/serializers.py
+++ questions/serializers.py
@@ -3,13 +3,21 @@ from questions.models import Question, Answer
 
 class AnswerSerializer(serializers.HyperlinkedModelSerializer):
     question = serializers.Field(source='question.text')
+    
+    # TODO: need to get the request here somehow to reverse the url, since apparently can't just do this
+    # question_url = serializers.Field(source='question.url')
+    
+    # hacking this for now...
+    question_url = serializers.SerializerMethodField('get_question_url')
+    def get_question_url(self, obj):
+        return "http://127.0.0.1:8000/questions/%d/" % obj.question.id
 
     class Meta:
         model = Answer
-        fields = ('url', 'value', 'is_correct', 'question')
+        fields = ('url', 'value', 'is_correct', 'question', 'question_url')
     
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
-    answers = serializers.HyperlinkedRelatedField(many=True, view_name='answer-detail')
+    answers = AnswerSerializer(many=True)
 
     class Meta:
         model = Question
